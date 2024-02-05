8731263fa83079e444b847d50c6d73699f88b762
CollabGuru <121675248+CollabGuru@users.noreply.github.com>
2024-02-05 12:22:21 -0500

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Github actions change - TODO: delete this comment
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
